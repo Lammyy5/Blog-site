83c7f416d0de765196f0723799dbf4442e4fc865
Lammyy5 <reliford2010@gmail.com>
2023-12-09 09:31:31 -0800

    added a post model and example seed data

--- models/index.js
+++ models/index.js
@@ -1,3 +1,11 @@
 const User = require('./User');
 
+User.hasMany(Post, {
+	foreignKey: 'user_id', // Set up the foreign key relationship
+})
+
+Post.belongsTo(User, {
+	foreignKey: 'user_id', // Set up the foreign key relationship
+})
+
 module.exports = { User };
